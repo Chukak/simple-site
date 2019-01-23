1018ed58ee9b86f45d4dcf9f477217336f7a3e6a
Chukak <chukak12@gmail.com>
2019-01-23 23:34:33 +0300

    fixed 'Username' and 'Password' fields

--- models/views/account/RegisterModel.cs
+++ models/views/account/RegisterModel.cs
@@ -4,44 +4,36 @@ namespace testsite.models.views.account
 {
     public class RegisterModel
     {
+        private string _username;
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [RegularExpression(@"^[^-\s]*$")]
         [Required]
-        [StringLength(80, ErrorMessage = "The password must be at least 80 character long.")]
+        [StringLength(80, MinimumLength = 6, 
+        ErrorMessage = "The password must be between 6 and 80 characters long.")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
-        public string Password
-        {
-            get 
-            {
-                return Password;
-            }
-            set
-            {
-                if (value.IndexOf(" ") == -1) {
-                    Password = value;
-                }
-            }
-        }
-
+        public string Password { get; set; }
+        
         [StringLength(100, ErrorMessage = "The username must be at least 100 character long.")]
         [DataType(DataType.Text)]
         [Display(Name = "Username")]
-        public string Username
+        public string Username 
         {
             get 
             {
-                return Username;
+                return _username;
             }
             set
             {
                 if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value)) {
-                    Username = Email;
+                    _username = Email;
                 } else {
-                    Username = value;
+                    _username = value;
                 }
             }
         }
